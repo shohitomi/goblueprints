b85f94e7bd9e4b30c72e3afc3ceda7c30b7d16b4
sho <shohitomi25@gmail.com>
2023-09-06 21:06:34 +0900

    added avatar to room

--- chat/main.go
+++ chat/main.go
@@ -49,7 +49,7 @@ func main() {
 		google.New(*googleClientID, *googleClientSecret, "http://localhost:8080/auth/callback/google"),
 	)
 
-	r := newRoom()
+	r := newRoom(UseAuthAvatar)
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
