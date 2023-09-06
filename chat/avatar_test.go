19add0f54b19cda54fe93e317eeb94aba76efe1a
sho <shohitomi25@gmail.com>
2023-09-06 22:07:16 +0900

    added FileSystemAvatar

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -35,5 +37,22 @@ func TestGravatarAvatar(t *testing.T) {
 	}
 	if url != "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
 		t.Errorf("GravatarAvatar.AvatarURL wrongly returned %s", url)
+	}
+}
+
+func TestFileSystemAvatar(t *testing.T) {
+	filename := filepath.Join("avatars", "abc.jpg")
+	os.WriteFile(filename, []byte{}, 0644)
+	defer func() { os.Remove(filename) }()
+
+	var fileSystemAvatar FileSystemAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"userid": "abc"}
+	url, err := fileSystemAvatar.AvatarURL(client)
+	if err != nil {
+		t.Error("FileSystemAvatar.AvatarURL should not return an error")
+	}
+	if url != "/avatars/abc.jpg" {
+		t.Errorf("FileSystemAvatar.AvatarURL wrongly returned %s", url)
 	}
 }
