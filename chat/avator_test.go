fefe3afb51a4c18c7604952311659e4368116f88
sho <shohitomi25@gmail.com>
2023-09-06 20:58:44 +0900

    fixed test

--- chat/avator_test.go
+++ chat/avator_test.go
@@ -7,14 +7,14 @@ import (
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
 	client := new(client)
-	url, err := authAvatar.AvatarURL(client)
+	_, err := authAvatar.AvatarURL(client)
 	if err != ErrNoAvatarURL {
 		t.Error("AuthAvatar.AvatarURL should return ErrNoAvatarURL when no value present")
 	}
 
 	testUrl := "http://url-to-avatar/"
 	client.userData = map[string]interface{}{"avatar_url": testUrl}
-	url, err = authAvatar.AvatarURL(client)
+	url, err := authAvatar.AvatarURL(client)
 	if err != nil {
 		t.Error("AuthAvatar.AvatarURL should return no error when value present")
 	}
