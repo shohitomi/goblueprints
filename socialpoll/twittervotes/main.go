1f359a389062f1f1f9f687dce7b211771ef1a125
sho <shohitomi25@gmail.com>
2023-09-08 11:36:19 +0900

    created db functions

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -1,3 +1,38 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dial to MongoDB")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("DB connection is closed")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
 func main() {}
